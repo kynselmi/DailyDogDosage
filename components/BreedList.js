cc7c42b98be7c6c67d66f3b2e26030309702a566
Aleksi A Kärkkäinen <aleksi.a.karkkainen@tieto.com>
2018-10-10 19:03:29 +0300

    Added sub-breeds to the list

--- components/BreedList.js
+++ components/BreedList.js
@@ -10,20 +10,39 @@ export class BreedList extends React.Component{
         await fetch('https://dog.ceo/api/breeds/list/all')
         .then(response => response.json())
         .then(json => {
-          this.setState({ breeds: json.message })
+          this.setState({ breeds: this.createBreedArray(json.message) })
         });
+        
     }
 
     capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
-    }   
+    }
+
+    createBreedArray(breedsInput) {
+        let breedArray = [];
+        for (breed in breedsInput) {
+            let childBreeds = breedsInput[breed];
+            if(childBreeds.length > 1) {
+                for (let i=0; i<childBreeds.length; i++) {
+                breedArray.push(this.capitalizeFirstLetter(childBreeds[i])+" "+this.capitalizeFirstLetter(breed));
+                }
+            } else {
+            breedArray.push(this.capitalizeFirstLetter(breed));
+            }
+        }
+        breedArray.sort();
+        return breedArray;
+    }
 
     render() {
         return (
             <FlatList style={this.props.style}
-            data={Object.keys(this.state.breeds)}
+            data={this.state.breeds}
             renderItem={({item, index}) => (
-                <Text style={this.props.rowStyle}>{this.capitalizeFirstLetter(item)+" "+this.state.breeds[item]}</Text>
+                <Text style={this.props.rowStyle}>
+                    {item}
+                </Text>
                 )}
             keyExtractor={(item, index) => index.toString()}
             ItemSeparatorComponent={() => <View style={this.props.separatorStyle} />}
